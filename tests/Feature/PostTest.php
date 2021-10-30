25ca45a2f687b829ecb4446264f06617aa03216a
fachri-literasia <fachri.amin@literasia.co.id>
2021-10-30 10:51:59 +0700

    finish post test

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -50,57 +50,175 @@ class PostTest extends TestCase
             'category_id' => $category->id,
             'user_id' => $user->id
         ]);
+        $tag = Tag::factory()->create();
+        $post->tags()->attach($tag->id);
 
         $response = $this->get('api/posts/'.$post->id);
-
-        $response->assertSee($post->name);
+        $response->assertSee($post->title);
+        $response->assertSee($post->body);
+        $response->assertSee($category->name);
+        $response->assertSee($tag->name);
+        $response->assertSee($user->email);
         $response->assertJsonStructure([
             'status',
             'message',
-            'data',
+            'data' => [
+                'id',
+                'title',
+                'body',
+                'category_id',
+                'user_id',
+                'created_at',
+                'updated_at',
+                'category',
+                'tags',
+                'user',
+            ],
         ]);
     }
 
     /** @test */
     public function can_create_new_post () {
+        $this->withoutExceptionHandling();
+        $user = User::factory()->create();
+        $this->actingAs($user);
         $post = Post::factory()->make();
+        $category = Category::factory()->create();
+        $tag = Tag::factory()->create();
 
-        $response = $this->post('api/posts', $post->toArray());
+        $request_data = [
+            'title' => $post->title,
+            'body' => $post->body,
+            'category_id' => $category->id,
+            'tags' => [$tag->id]
+        ];
+
+        // dd($request_data);
+
+        $response = $this->post('api/posts', $request_data);
         $this->assertEquals(1, Post::all()->count());
-        $response->assertSee('Success Add Data post');
+        $response->assertSee($request_data['title']);
+        $response->assertSee($request_data['body']);
+        $response->assertSee($user->id);
+        $response->assertSee($request_data['category_id']);
+        $response->assertJsonStructure([
+            'status',
+            'message',
+            'data' => [
+                'id',
+                'title',
+                'body',
+                'category_id',
+                'user_id',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
     }
 
     /** @test */
-    public function a_new_post_requires_name () {
-        $post = Post::factory()->make(['name' => null]);
+    public function a_new_post_requires_title () {
+        $post = Post::factory()->make(['title' => null]);
         $this->post('api/posts', $post->toArray())
-            ->assertStatus(422);
+            ->assertStatus(302); // ini 302 karena pake request class sendiri
+    }
+
+    /** @test */
+    public function a_new_post_requires_body () {
+        $post = Post::factory()->make(['body' => null]);
+        $this->post('api/posts', $post->toArray())
+            ->assertStatus(302);
+    }
+    
+    /** @test */
+    public function a_new_post_requires_category_id () {
+        $post = Post::factory()->make(['category_id' => null]);
+        $this->post('api/posts', $post->toArray())
+            ->assertStatus(302);
+    }
+
+    /** @test */
+    public function unauthenticate_user_can_not_create_post () {
+        // $user = User::factory()->create();
+        // $this->actingAs($user);
+        $post = Post::factory()->make();
+        $category = Category::factory()->create();
+        $tag = Tag::factory()->create();
+
+        $request_data = [
+            'title' => $post->title,
+            'body' => $post->body,
+            'category_id' => $category->id,
+            'tags' => [$tag->id]
+        ];
+
+        // dd($request_data);
+
+        $this->json('POST','api/posts', $request_data, ['Accept' => 'application/json'])
+            ->assertStatus(401);
+        // $this->assertNotAuthenticated();
     }
 
     /** @test */
     public function can_edit_post () {
-        $post = Post::factory()->create();
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $category = Category::factory()->create();
+        $post = Post::factory()->create(['category_id' => $category->id]);
 
-        $post->name = 'post name updated';
+        $post->title = 'post title updated';
+        $post->save();
 
         $response = $this->put('api/posts/'.$post->id, $post->toArray());
 
-        $this->assertDatabaseHas('posts', ['id' => $post->id, 'name' => 'post name updated']);
+        $this->assertDatabaseHas('posts', ['id' => $post->id, 'title' => 'post title updated']);
     }
 
     /** @test */
-    public function updated_post_requires_name () {
-        $post = Post::factory()->create();
+    public function updated_post_requires_title () {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $category = Category::factory()->create();
+        $post = Post::factory()->create(['category_id' => $category->id]);
 
-        $post->name = null;
+        $post->title = null;
 
         $response = $this->put('api/posts/'.$post->id, $post->toArray())
-            ->assertStatus(422);
+            ->assertStatus(302);
+    }
+
+    /** @test */
+    public function updated_post_requires_body () {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $category = Category::factory()->create();
+        $post = Post::factory()->create(['category_id' => $category->id]);
+
+        $post->body = null;
+
+        $response = $this->put('api/posts/'.$post->id, $post->toArray())
+            ->assertStatus(302);
+    }
+
+    /** @test */
+    public function updated_post_requires_category_id () {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $category = Category::factory()->create();
+        $post = Post::factory()->create(['category_id' => $category->id]);
+
+        $post->category_id = null;
+
+        $response = $this->put('api/posts/'.$post->id, $post->toArray())
+            ->assertStatus(302);
     }
 
     /** @test */
     public function can_delete_post () {
-        $post = Post::factory()->create();
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $category = Category::factory()->create();
+        $post = Post::factory()->create(['category_id' => $category->id]);
 
         $response = $this->delete('posts/'.$post->id);
         $this->assertDatabaseMissing('posts', $post->toArray());
